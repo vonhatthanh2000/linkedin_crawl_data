8e66bac391f025559b68833998bf17dc4a1d4ad7
thanh <nthanhatee@gmail.com>
2023-03-12 11:54:14 +0700

    first commit

---
+++ basic_scrapy_spider/spiders/quotes.py
@@ -0,0 +1,205 @@
+import scrapy
+import json
+
+class LinkedInPeopleProfileSpider(scrapy.Spider):
+    name = "linkedin_people_profile"
+
+    custom_settings = {
+        'FEEDS': { 'data/%(name)s_%(time)s.jsonl': { 'format': 'jsonlines',}}
+        }
+
+    def start_requests(self):
+        profile_list = ['reidhoffman']
+        for profile in profile_list:
+            linkedin_people_url = 'https://www.linkedin.com/in/reidhoffman/' 
+            yield scrapy.Request(url=linkedin_people_url, callback=self.parse_profile, meta={'profile': profile, 'linkedin_url': linkedin_people_url})
+
+    def parse_profile(self, response):
+        item = {}
+        item['profile'] = response.meta['profile']
+        item['url'] = response.meta['linkedin_url']
+
+        """
+            SUMMARY SECTION
+        """
+        summary_box = response.css("section.top-card-layout")
+        item['name'] = summary_box.css("h1::text").get().strip()
+        item['description'] = summary_box.css("h2::text").get().strip()
+
+        ## Location
+        try:
+            item['location'] = summary_box.css('div.top-card__subline-item::text').get()
+        except:
+            item['location'] = summary_box.css('span.top-card__subline-item::text').get().strip()
+            if 'followers' in item['location'] or 'connections' in item['location']:
+                item['location'] = ''
+
+        item['followers'] = ''
+        item['connections'] = ''
+
+        for span_text in summary_box.css('span.top-card__subline-item::text').getall():
+            if 'followers' in span_text:
+                item['followers'] = span_text.replace(' followers', '').strip()
+            if 'connections' in span_text:
+                item['connections'] = span_text.replace(' connections', '').strip()
+
+
+        """
+            ABOUT SECTION
+        """
+        item['about'] = response.css('section.summary div.core-section-container__content p::text').get(default='')
+
+
+        """
+            EXPERIENCE SECTION
+        """
+        item['experience'] = []
+        experience_blocks = response.css('li.experience-item')
+        for block in experience_blocks:
+            experience = {}
+            ## organisation profile url
+            experience['organisation_profile'] = block.css('h4 a::attr(href)').get(default='').split('?')[0]
+                
+                
+            ## location
+            experience['location'] = block.css('p.experience-item__location::text').get(default='').strip()
+                
+                
+            ## description
+            try:
+                experience['description'] = block.css('p.show-more-less-text__text--more::text').get().strip()
+            except Exception as e:
+                print('experience --> description', e)
+                try:
+                    experience['description'] = block.css('p.show-more-less-text__text--less::text').get().strip()
+                except Exception as e:
+                    print('experience --> description', e)
+                    experience['description'] = ''
+                    
+            ## time range
+            try:
+                date_ranges = block.css('span.date-range time::text').getall()
+                if len(date_ranges) == 2:
+                    experience['start_time'] = date_ranges[0]
+                    experience['end_time'] = date_ranges[1]
+                    experience['duration'] = block.css('span.date-range__duration::text').get()
+                elif len(date_ranges) == 1:
+                    experience['start_time'] = date_ranges[0]
+                    experience['end_time'] = 'present'
+                    experience['duration'] = block.css('span.date-range__duration::text').get()
+            except Exception as e:
+                print('experience --> time ranges', e)
+                experience['start_time'] = ''
+                experience['end_time'] = ''
+                experience['duration'] = ''
+            
+            item['experience'].append(experience)
+
+        
+        """
+            EDUCATION SECTION
+        """
+        item['education'] = []
+        education_blocks = response.css('li.education__list-item')
+        for block in education_blocks:
+            education = {}
+
+            ## organisation
+            education['organisation'] = block.css('h3::text').get(default='').strip()
+
+
+            ## organisation profile url
+            education['organisation_profile'] = block.css('a::attr(href)').get(default='').split('?')[0]
+
+            ## course details
+            try:
+                education['course_details'] = ''
+                for text in block.css('h4 span::text').getall():
+                    education['course_details'] = education['course_details'] + text.strip() + ' '
+                education['course_details'] = education['course_details'].strip()
+            except Exception as e:
+                print("education --> course_details", e)
+                education['course_details'] = ''
+
+            ## description
+            education['description'] = block.css('div.education__item--details p::text').get(default='').strip()
+
+
+         
+            ## time range
+            try:
+                date_ranges = block.css('span.date-range time::text').getall()
+                if len(date_ranges) == 2:
+                    education['start_time'] = date_ranges[0]
+                    education['end_time'] = date_ranges[1]
+                elif len(date_ranges) == 1:
+                    education['start_time'] = date_ranges[0]
+                    education['end_time'] = 'present'
+            except Exception as e:
+                print("education --> time_ranges", e)
+                education['start_time'] = ''
+                education['end_time'] = ''
+
+            item['education'].append(education)
+
+        yield item
+
+class LinkedCompanySpider(scrapy.Spider):
+    name = "linkedin_company_profile"
+
+    #add your own list of company urls here
+    company_pages = [
+        'https://www.linkedin.com/company/usebraintrust',
+        'https://www.linkedin.com/company/codelightco',
+        ]
+
+
+    def start_requests(self):
+        
+        company_index_tracker = 0
+
+        first_url = self.company_pages[company_index_tracker]
+
+        yield scrapy.Request(url=first_url, callback=self.parse_response, meta={'company_index_tracker': company_index_tracker})
+
+
+    def parse_response(self, response):
+        company_index_tracker = response.meta['company_index_tracker']
+        print('***************')
+        print('****** Scraping page ' + str(company_index_tracker+1) + ' of ' + str(len(self.company_pages)))
+        print('***************')
+
+        company_item = {}
+
+        company_item['name'] = response.css('.top-card-layout__entity-info h1::text').get(default='not-found').strip()
+        company_item['summary'] = response.css('.top-card-layout__entity-info h4 span::text').get(default='not-found').strip()
+
+        try:
+            ## all company details 
+            company_details = response.css('.core-section-container__content .mb-2')
+
+            #industry line
+            company_industry_line = company_details[1].css('.text-md::text').getall()
+            company_item['industry'] = company_industry_line[1].strip()
+
+            #company size line
+            company_size_line = company_details[2].css('.text-md::text').getall()
+            company_item['size'] = company_size_line[1].strip()
+
+            #company founded
+            company_size_line = company_details[5].css('.text-md::text').getall()
+            company_item['founded'] = company_size_line[1].strip()
+        except IndexError:
+            print("Error: Skipped Company - Some details missing")
+
+        yield company_item
+        
+
+        company_index_tracker = company_index_tracker + 1
+
+        if company_index_tracker <= (len(self.company_pages)-1):
+            next_url = self.company_pages[company_index_tracker]
+
+            yield scrapy.Request(url=next_url, callback=self.parse_response, meta={'company_index_tracker': company_index_tracker})
+    
+
